21de4b829526c541bce3924601f449223eb71445
Carl James Tolarba <157483498+carljames18@users.noreply.github.com>
2024-06-05 06:39:28 +0800

    Add files via upload

--- checkout.php
+++ checkout.php
@@ -1,201 +1,128 @@
 <?php 
-
 session_start();
 
-if( !empty($_SESSION['cart'] ) && isset($_POST['checkout'])){
-
-    //let user in
-
-
-
-    //send user to homepage
-}else{
-
+if (!empty($_SESSION['cart'])) {
+    // Let the user proceed to checkout
+} else {
     header('location: index.php');
-
 }
-
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Checkout</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<?php include('layouts/header.php'); ?>
 
-    <link rel="stylesheet" href="assets/css/style.css"/>
-
-
-</head>
-<body>
-
-    <!--Navbar-->
-    <nav class="navbar navbar-expand-lg navbar-light bg-skyblue py-3 fixed-top">
-        <div class="container">
-          <img class="logo" src="assets/imgs/logoo.png"/>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse nav-buttons" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link" href="index.php">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="shop.html">Shop</a>
-              </li>
-
-              <li class="nav-item">
-                <a class="nav-link" href="#">Blog</a>
-              </li>
-
-              <li class="nav-item">
-                <a class="nav-link" href="contact.html">Contact Us</a>
-              </li>
-
-
-              <li class="nav-item">
-                <a class="nav-link" href="cart.php">Cart</a>
-              </li>
-
-              <li class="nav-item">
-                <a class="nav-link" href="account.html">Account</a>
-              </li>
-
-
-
-
-            </ul>
-          </div>
-        </div>
-      </nav>
-
-
-    <!--Checkout-->
-    <section class="my-5 py-5">
-        <div class="container text-center mt-3 pt-5">
-            <h2 class="form-weight-bold">Checkout</h2>
-            <hr class="checkout-hr mx-auto">
-        </div>
-        <div class="mx-auto container">
-            <form id="checkout-form" method="POST" action="server/place_order.php">
-                <div class="form-group checkout-small-element">
-                    <label>Full Name</label>
-                    <input type="text" class="form-control" id="checkout-fullname" name="name" placeholder="Enter Your Full Name" required/>
-                </div>
-
-                <div class="form-group checkout-small-element">
-                    <label>BU Email</label>
-                    <input type="text" class="form-control" id="checkout-email" name="email" placeholder="Enter Your BU Email" required/>
-                </div>
-
-                <div class="form-group checkout-small-element">
-                    <label>Phone Number</label>
-                    <input type="text" class="form-control" id="checkout-phone-number" name="phone" placeholder="Enter Your Active Phone Number" required/>
-                </div>
-
-                <div class="form-group checkout-small-element">
-                    <label>Campus</label>
-                    <input type="text" class="form-control" id="checkout-campus" name="campus" placeholder="Ex: BUP" required/>
-                </div>
-
-                <div class="form-group checkout-small-element">
-                    <label>Course</label>
-                    <input type="text" class="form-control" id="checkout-course" name="course" placeholder="Enter your Course" required/>
-                </div>
-
-                <div class="form-group checkout-small-element">
-                    <label>City / Municipality</label>
-                    <input type="text" class="form-control" id="checkout-city" name="city" placeholder="Enter Your City / Municipality" required/>
-                </div>
-
-                <div class="form-group checkout-large-element">
-                    <label>Address</label>
-                    <input type="text" class="form-control" id="checkout-address" name="address" placeholder="Enter Your Full Address" required/>
-                </div>
-                <div class="form-group checkout-btn-container">
-                    <p>Total amount: Php <?php echo $_SESSION['total']?>.00</p>
-                    <input type="submit" class="btn" id="checkout-btn" name="place_order" value="Place Order"/>
-                </div>
-            </form>
-        </div>
-    </section>
-
-
-
-
-
-
-
-      <!--Footer-->
-      <footer class="mt-5 py-5">
-        <div class="row container mx-auto pt-5">
-            <div class="footer-one col-lg-3 col-md-6 col-sm-12">
-                <img class="logo" src="assets/imgs/logoo.png"/>
-                <p class="pt-3">We provide the best product for the most affordable prices</p>
+<section class="my-5 py-5">
+    <div class="container text-center mt-3 pt-5">
+        <h2 class="form-weight-bold">Checkout</h2>
+        <hr class="checkout-hr mx-auto">
+    </div>
+    <div class="mx-auto container">
+        
+        <form id="checkout-form" method="POST" action="payment.php">
+            <p class="text-center" style="color: orange;"> 
+              <?php if(isset($_GET['message'])) { echo $_GET['message']; } ?>
+              <?php if(isset($_GET['message'])) { ?>
+                   <a href="login.php" class="btn btn-primary">Login</a>
+                <?php } ?>
+            </p>
+            <div class="form-group checkout-small-element">
+                <label>Full Name</label>
+                <input type="text" class="form-control" id="checkout-fullname" name="name" placeholder="Enter Your Full Name" required/>
             </div>
-    
-            <div class="footer-one col-lg-3 col-md-6 col-sm-12">
-                <h5 class="pb-2">Featured</h5>
-                <ul class="text-uppercase">
-                    <li><a href="#">Shirts</a></li>
-                    <li><a href="#">Lanyards</a></li>
-                    <li><a href="#">Totebags</a></li>
-                    <li><a href="#">Stickers</a></li>
-                    <li><a href="#">New Release</a></li>
-                </ul>
+
+            <div class="form-group checkout-small-element">
+                <label>BU Email</label>
+                <input type="text" class="form-control" id="checkout-email" name="email" placeholder="Enter Your BU Email" required/>
             </div>
-    
-            <div class="footer-one col-lg-3 col-md-6 col-sm-12">
-                <h5 class="pb-2">Contact Us</h5>
-                <div>
-                    <h6 class="text-uppercase">Address:</h6>
-                    <p>Rizal Street, Legazpi City, Albay, Legazpi, Philippines</p>
+
+            <div class="form-group checkout-small-element">
+                <label>Phone Number</label>
+                <input type="text" class="form-control" id="checkout-phone-number" name="phone" placeholder="Enter Your Active Phone Number" required/>
+            </div>
+
+            <div class="form-group checkout-small-element">
+                <label>Campus</label>
+                <input type="text" class="form-control" id="checkout-campus" name="campus" placeholder="Ex: BUP" required/>
+            </div>
+
+            <div class="form-group checkout-small-element">
+                <label>Course</label>
+                <input type="text" class="form-control" id="checkout-course" name="course" placeholder="Enter your Course" required/>
+            </div>
+
+            <div class="form-group checkout-small-element">
+                <label>City / Municipality</label>
+                <input type="text" class="form-control" id="checkout-city" name="city" placeholder="Enter Your City / Municipality" required/>
+            </div>
+
+            <div class="form-group checkout-large-element">
+                <label>Address</label>
+                <input type="text" class="form-control" id="checkout-address" name="address" placeholder="Enter Your Full Address" required/>
+            </div>
+
+            <div class="form-group checkout-small-element">
+                <label>Payment Method</label><br>
+                <input type="radio" id="cod" name="payment_method" value="cod" required>
+                <label for="cod">Cash on Delivery</label><br>
+                <input type="radio" id="gcash" name="payment_method" value="gcash" required>
+                <label for="gcash">GCash</label>
+            </div>
+
+            <!-- Additional fields for GCash payment -->
+            <div id="gcash-fields" style="display: none;">
+            <p>G-CASH ACCT NAME: BUMAKAL NA KAMO</p>
+            <p>ACCOUNT NUMBER: 09282040817</p>
+                <div class="form-group">
+                    <label>Account Name</label>
+                    <input type="text" class="form-control" id="account-name" name="account_name" placeholder="Enter Your GCash Account Name" required>
                 </div>
-                <div>
-                    <h6 class="text-uppercase">Contact Number:</h6>
-                    <p>090909090909 / 09324142342</p>
+                <div class="form-group">
+                    <label>Account Number</label>
+                    <input type="text" class="form-control" id="account-number" name="account_number" placeholder="Enter Your GCash Account Number" required>
                 </div>
-                <div>
-                    <h6 class="text-uppercase">Email Address:</h6>
-                    <p>admin@bicol-u.edu.ph</p>
+                <div class="form-group">
+                    <label>Amount</label>
+                    <input type="number" class="form-control" id="amount" name="amount" placeholder="Enter Amount" required>
+                </div>
+                <div class="form-group">
+                    <label>Reference Number</label>
+                    <input type="text" class="form-control" id="reference-number" name="reference_number" placeholder="Enter Reference Number" required>
                 </div>
             </div>
-    
-            <div class="footer-one col-lg-3 col-md-6 col-sm-12 ">
-                <h5 class="pb-2">Instagram</h5>
-                <div class="row">
-                    <img src="assets/imgs/footer1.jpeg" class="img-fluid w-25 h-100 m-2"/>
-                    <img src="assets/imgs/footer1.jpeg" class="img-fluid w-25 h-100 m-2"/>
-                    <img src="assets/imgs/footer1.jpeg" class="img-fluid w-25 h-100 m-2"/>
-                    <img src="assets/imgs/footer1.jpeg" class="img-fluid w-25 h-100 m-2"/>
-                    <img src="assets/imgs/footer1.jpeg" class="img-fluid w-25 h-100 m-2"/>
-                </div>
+            
+            <div class="form-group checkout-btn-container">
+                <p>Total amount: Php <?php echo $_SESSION['total'] ?>.00</p>
+                <input type="submit" class="btn" id="checkout-btn" name="place_order" value="Place Order"/>
             </div>
-        </div>
-    
-        <div class="copyright mt-5">
-            <div class="row container mx-auto">
-                <div class="col-lg-3 col-md-5 col-sm-12 mb-4">
-                    <img src="assets/imgs/payment.png"/>
-                    <img src="assets/imgs/Gcash.png"/>
-                </div>
-                <div class="col-lg-3 col-md-5 col-sm-12 mb-4 text-nowrap mb-2">
-                    <p>BUMAKAL@2024 All Right Reserved</p>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-12 mb-4"> 
-                    <a href="#"><i class="fab fa-facebook"></i> </a>
-                    <a href="#"><i class="fab fa-instagram"></i> </a>
-                    <a href="#"><i class="fab fa-x"></i> </a>
-                </div>
-            </div>
-        </div>
-        </footer>
+        </form>
+    </div>
+</section>
 
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
-</body>
-</html>    
+<?php include('layouts/footer.php'); ?>
+
+<script>
+
+document.getElementById('checkout-form').addEventListener('submit', function(event) {
+    var paymentMethod = document.querySelector('input[name="payment_method"]:checked').value;
+    if (paymentMethod === 'gcash') {
+        var accountName = document.getElementById('account-name').value.trim();
+        var accountNumber = document.getElementById('account-number').value.trim();
+        var amount = document.getElementById('amount').value.trim();
+        var referenceNumber = document.getElementById('reference-number').value.trim();
+        
+        if (!accountName || !accountNumber || !amount || !referenceNumber) {
+            event.preventDefault();
+            alert('Please fill in all required fields for GCash payment.');
+        }
+    }
+});
+
+document.querySelectorAll('input[name="payment_method"]').forEach(function(radio) {
+    radio.addEventListener('change', function() {
+        if (this.value === 'gcash') {
+            document.getElementById('gcash-fields').style.display = 'block';
+        } else {
+            document.getElementById('gcash-fields').style.display = 'none';
+        }
+    });
+});
+</script>
